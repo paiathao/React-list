2866044e24bc8559ca46a406e31e7ed150186385
Paia Thao <paiathao09@gmail.com>
2018-07-27 15:40:36 -0500

    add material iu

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,21 @@ import StarList from '../StarList/StarList'
 import NewStarForm from '../NewStarForm/NewStarForm'
 import axios from 'axios'
 import PlanetList from '../PlanetList/PlanetList'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import {createMuiTheme} from '@material-ui/core/styles'
+import red from '@material-ui/core/colors'
+import 'typeface-roboto'
+
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: red,
+    secondary: {
+      main: '#00ACB0',
+    },
+  },
+});
 
 class App extends Component {
   constructor(props) {
@@ -113,6 +128,7 @@ class App extends Component {
     // })
 
     return (
+      <MuiThemeProvider theme={theme} >
       <div className="App">
       <Header/>
       <CurrentNewStar currentStar={this.state.newStar}/>
@@ -125,6 +141,7 @@ class App extends Component {
       <PlanetList listOfPlanets = {this.state.planetList}/>
       <Footer/>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import {Call} from '@material-ui/icons'
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
 
 class Star extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      isVisible: true
+      isVisible: true,
+      open: false,
     }
   }
+
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+
 
   handleClick = (event) => {
     this.setState({
@@ -39,8 +62,36 @@ render() {
   return (
     <div>
       {listItem}
-      <button onClick={this.handleClick}>Show/Hide</button>
-      <button onClick={this.handleGetMoreInfo}>Click for More Info!</button>
+      <Button color="secondary" variant="outlined" onClick={this.handleClick}>Show/Hide<Call/></Button>
+      <Button color="primary" variant="flat" onClick={this.handleGetMoreInfo}>Click for More Info!</Button>
+
+      <Button onClick={this.handleClickOpen}>Slide in alert dialog</Button>
+        <Dialog
+          open={this.state.open}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle id="alert-dialog-slide-title">
+            {"Nunki's location service?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description">
+              Let Google help apps determine location. This means sending anonymous location data to
+              Google, even when no apps are running.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Disagree
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Agree
+            </Button>
+            </DialogActions>
+        </Dialog>
     </div>
   );
 }
